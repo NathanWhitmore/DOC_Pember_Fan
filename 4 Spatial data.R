b63ef0dd87819b4f63fec02cf2dee76e4f44fb1c
Nathan Whitmore <nathan@reproducible.co.nz>
2026-09-11 12:12:27 +1200

    update

--- 4 Spatial data.R
+++ 4 Spatial data.R
@@ -4,10 +4,10 @@ library(ggrepel)
 library(ggspatial)
 library(sf)
 library(glmmTMB)
+library(spdep)
 
 convenant <- st_read("Lees_Valley_Convenant_Transects_20191220.gpx")
 reserve <-st_read("Lees_Valley_Rec_Reserve_transects_20191220.~dbf.gpx")
-
 
 ggplot()+
   theme_void()+
@@ -45,75 +45,13 @@ my.coord  <- my.coord  |>
 my.data <- left_join(no.species.wide, my.coord, by = "Plot") %>% as.data.frame()
 str(my.data )
 
-m.test1 <- glmmTMB( Indigenous ~ Year * Exotic + (1 | Plot/Subplot) + exp(pos + 0 | spatial_group),
-                    family = genpois(),
-                    data = my.data
-)
-
-m.test2 <- glmmTMB( Indigenous ~ Year + Exotic + (1 | Plot) + exp(pos + 0 | spatial_group),
-                    family = genpois(),
-                    data = my.data
-)
+# m.test1 <- glmmTMB( Indigenous ~ Year * Exotic + (1 | Plot/Subplot) + exp(pos + 0 | spatial_group),
+#                    family = genpois(),
+#                    data = my.data
+#)
 
 
-diagnose(m.test2)
-
-m.plot <- glmmTMB(
-  Indigenous ~ Year + Exotic +
-    (1 | Plot),
-  family = genpois(),
-  data = my.data
-)
-
-m.spatial <- glmmTMB(
-  Indigenous ~ Year + Exotic +
-    exp(pos + 0 | spatial_group),
-  family = genpois(),
-  data = my.data
-)
-
-m.spatial <- glmmTMB(
-  Indigenous ~ Year + Exotic +
-    exp(pos + 0 | spatial_group),
-  family = genpois(),
-  data = my.data
-)
-
-m.spatial2 <- glmmTMB(
-  Indigenous ~ Year * Exotic +
-    exp(pos + 0 | spatial_group)+
-    (1 | Plot:Subplot),
-  family = genpois(),
-  data = my.data
-)
-
-
-VarCorr(m.spatial2)
-
-diagnose(m.plot)
-diagnose(m.spatial)
-
-VarCorr(m.plot)
-VarCorr(m.spatial)
-
-
-VarCorr(m.test2)
-
-AIC(m.plot, m.spatial)
-logLik(m.plot)
-logLik(m.spatial)
-
-
-# Is position effectively equivalent to Plot?
-my.data |>
-  distinct(Plot, pos) |>
-  count(Plot)
-
-
-m.spatial2$fit$par
-
-summary(m.spatial2)
-
+# checking spatial distribution of random effects
 
 m <- glmmTMB(
   Indigenous ~ Year + (1 | Plot/Subplot),
@@ -121,6 +59,7 @@ m <- glmmTMB(
   data = my.data
 )
 
+re <- ranef(m)$cond$Plot %>% as.data.frame()
 
 plot.re <- data.frame(
   Plot = rownames(re),
@@ -135,8 +74,6 @@ nrow(coords)
 
 # rejoin
 plot.re <- left_join(plot.re, coords, by = "Plot")
-
-library(spdep)
 
 # make distnace matrix
 xy <- cbind(plot.re$X, plot.re$Y)
@@ -200,7 +137,7 @@ viz$Group <- substr(viz$Plot, 1, 2)
 river.lines <- st_read("river/nz-river-name-lines-pilot.shp")
 river.polygons <- st_read("river/nz-river-polygons-topo-150k.shp")
 
-adj.viz <- st_bbox(  c(xmin=  1533588 ,
+adj.viz <- st_bbox(  c(xmin =  1533588 ,
           ymin = 5224676 ,
           xmax = 1536088 + 600, 
           ymax  = (5227264 + 200)))
@@ -208,18 +145,17 @@ adj.viz <- st_bbox(  c(xmin=  1533588 ,
 river.lines.cropped <- st_crop(river.lines, st_bbox(adj.viz))
 river.polygons.cropped <- st_crop(river.polygons, st_bbox(adj.viz))
 
-ggplot()+
-  geom_sf(data = river)
-
 # read kml
 
 ashley <- st_read("Ashley river boundary.kml")
+fence <- st_read("fence line/Fence_line.json")
 
 ggplot()+
   theme_void()+
   geom_sf(data = river.lines.cropped)+
   geom_sf(data = river.polygons.cropped)+
   geom_sf(data = ashley, colour = "blue", lwd =2)+
+  geom_sf(data = fence, colour = "red")+
   geom_sf(data = viz, aes(colour =effect, fill = effect, shape = Group), colour = "grey", size =5)+
   scale_fill_gradient(low = "yellow", high = "purple")+
   scale_shape_manual(values = c(21, 23))+
@@ -227,56 +163,141 @@ ggplot()+
   annotation_north_arrow(location = "bl",
                          pad_x = unit(0.1, "cm"),
                          pad_y = unit(1, "cm"))+
-  ggtitle("Pember fan: random effecst for biodiversity appear spatially autocorrelated")+
+  ggtitle("Pember fan: random effects for biodiversity appear spatially autocorrelated")+
   theme(plot.title = element_text(size = 16, face =2, colour = "grey"))+
   labs(fill = "Random effect size", 
        shape = "Plot type",
-       subtitle = "model: indig.biodiv ~ Year + (1|Plot/Subplot), family = genpois()")
+       subtitle = "model: Indigenous ~ Year + (1|Plot/Subplot), family = genpois()")
 
 ggsave("Pember fan random effect spatial signal.png",  
        scale = 1.2, width =9, height = 6,
        bg = "white")
 
 
+# incorporate ashley river distance 
 my.data.sf <- st_as_sf(my.data, coords =c("X", "Y"), crs = 2193)
 ashley <- st_union(ashley) %>% st_transform(crs = 2193)
 
-my.data$river_dist <- as.numeric(
+# incorporate fence line 
+fence <- fence  %>% st_transform(crs = 2193)
+
+my.data$river.dist <- as.numeric(
   st_distance(my.data.sf, ashley )
 )
 
+my.data$fence.dist <- as.numeric(
+  st_distance(my.data.sf, fence )
+)
 
-m1 <- glmmTMB(
-  Indigenous ~ Year + (1 | Plot),
+# model selection
+
+Models <- list()
+
+Models[[1]] <- glmmTMB(Indigenous ~ 1 + (1|Plot/Subplot), family = genpois(), data = my.data)
+Models[[2]] <- glmmTMB(Indigenous ~ Year + (1|Plot/Subplot), family = genpois(), data = my.data)
+Models[[3]] <- glmmTMB(Indigenous ~ Exotic + (1|Plot/Subplot), family = genpois(), data = my.data)
+Models[[4]] <- glmmTMB(Indigenous ~ Year + Exotic + (1|Plot/Subplot), family = genpois(), data = my.data)  
+Models[[5]] <- glmmTMB(Indigenous ~ Year * Exotic + (1|Plot/Subplot), family = genpois(), data = my.data)
+Models[[6]] <- glmmTMB(Indigenous ~ Year + scale(river.dist) + (1|Plot/Subplot), family = genpois(), data = my.data) 
+Models[[7]] <- glmmTMB(Indigenous ~ Year + scale(fence.dist) + (1|Plot/Subplot), family = genpois(), data = my.data)
+Models[[8]] <- glmmTMB(Indigenous ~ scale(river.dist) + (1|Plot/Subplot), family = genpois(), data = my.data)
+Models[[9]] <- glmmTMB(Indigenous ~ scale(fence.dist) + (1|Plot/Subplot), family = genpois(), data = my.data)
+# Models[[7]] <- glmmTMB(Indigenous ~ Year + Exotic + scale(river_dist) + (1|Plot/Subplot), family = genpois(), data = my.data)
+#Models[[8]] <- glmmTMB(Indigenous ~ Year * scale(river_dist) + (1|Plot/Subplot), family = genpois(), data = my.data) 
+
+# create a vector of names to trace back models in set
+Modnames <- paste("mod", 1:length(Models), sep = " ")
+Modnames <- paste(sub(".*formula =*(.*?) *, .*", "\\1", 
+                      unlist(lapply(Models, formula))))
+
+# AIC table to 4 digits
+aictab(cand.set = Models, modnames = Modnames, sort = TRUE)
+
+# summary
+summary(Models[[7]])
+
+
+# simplified gen vs com
+gen <- glmmTMB(Indigenous ~ Year + scale(fence.dist) + (1|Plot), family = genpois(), data = my.data)
+com <- glmmTMB(Indigenous ~ Year + scale(fence.dist) + (1|Plot), family = compois(), data = my.data)
+
+# diagnostics
+res <- simulateResiduals(Models[[7]])
+res.gen <- simulateResiduals(gen)
+res.com <- simulateResiduals(com)
+
+# test dispersion compois performs much better
+testDispersion(res)
+testDispersion(res.gen)
+testDispersion(res.com)
+
+# test K-S test (ideally want to see 0) compois performs much better
+testUniformity(res)
+testUniformity(res.gen)
+testUniformity(res.com)
+
+# are subplot random effects warranted
+rand.without <- glmmTMB(Indigenous ~ Year + scale(fence.dist) + (1|Plot), family = genpois(), data = my.data, REML = TRUE)
+rand.with <- glmmTMB(Indigenous ~ Year + scale(fence.dist) + (1|Plot/Subplot), family = genpois(), data = my.data, REML = TRUE)
+
+# subplots are totally justified
+AIC(rand.without, rand.with)
+
+
+# new random effects
+m <- glmmTMB(
+  Indigenous ~ Year + scale(fence.dist) + (1 | Plot/Subplot),
   family = genpois(),
   data = my.data
 )
 
-m2 <- glmmTMB(
-  Indigenous ~ Year + river_dist+ (1 | Plot) +
-    (1 | Plot:Subplot),
-  family = genpois(),
-  data = my.data
+re <- ranef(m)$cond$Plot %>% as.data.frame()
+
+plot.re <- data.frame(
+  Plot = rownames(re),
+  effect = re[, "(Intercept)"]
 )
 
-summary(m2)
-ranef(m3)
+coords <- my.data %>%
+  distinct(Plot, X, Y)
 
-m3 <- glmmTMB(
-  Indigenous ~ Year + river_dist,
-  family = genpois(),
-  data = my.data
-)
-summary(m3)
+# check there are 30 rows
+nrow(coords)
 
-glmmTMB::diagnose(m3)
+# rejoin
+plot.re <- left_join(plot.re, coords, by = "Plot")
 
-AIC(m1,m2,m3)
-summary(m2)
+my.coord.sf <- my.coord %>% st_as_sf(coords = c("X", "Y"), crs = 2193)
+
+viz <- left_join(my.coord.sf, plot.re, by = "Plot")
+viz$Group <- substr(viz$Plot, 1, 2)
+
+# COM-Poisson provided somewhat better residual behaviour in simpler models, 
+# but was computationally prohibitive for the full hierarchical models; 
+# generalized-Poisson models were therefore used for the main analysis, 
+# with COM-Poisson fits to reduced models used to assess sensitivity of the principal conclusions.
+
+ggplot()+
+  theme_void()+
+  geom_sf(data = river.lines.cropped)+
+  geom_sf(data = river.polygons.cropped)+
+  geom_sf(data = ashley, colour = "blue", lwd =2)+
+  geom_sf(data = fence, colour = "red")+
+  geom_sf(data = viz , aes(colour =effect, fill = effect, shape = Group),colour = "grey", size =5)+
+  scale_fill_gradient(low = "yellow", high = "purple")+
+  scale_shape_manual(values = c(21, 23))+
+  annotation_scale(location = "bl")+
+  annotation_north_arrow(location = "bl",
+                         pad_x = unit(0.1, "cm"),
+                         pad_y = unit(1, "cm"))+
+  ggtitle("Pember fan: random effects for biodiversity appear spatially autocorrelated")+
+  theme(plot.title = element_text(size = 16, face =2, colour = "grey"))+
+  labs(fill = "Random effect size", 
+       shape = "Plot type",
+       subtitle = "model: Indigenous ~ Year + scale(fence.dist) + (1|Plot/Subplot), family = genpois()")
+
+ggsave("Pember fan random effect fence spatial.png",  
+       scale = 1.2, width =9, height = 6,
+       bg = "white")
 
 
-my.data <- my.data |>
-  mutate(
-    river_dist_z = as.numeric(scale(river_dist)),
-    Year_z = as.numeric(scale(Year))
-  )
